388ef68d2a17ff20a22a8cf510ae254302c8883b
Johannes Englisch <cyberjoe0815@hotmail.com>
2013-02-16 22:28:29 +0100

    fixed typos that prevented test from running

--- buildwebpagelib/subpage.py
+++ buildwebpagelib/subpage.py
@@ -1,3 +1,4 @@
+import re
 from . import cfg
 
 
@@ -7,17 +8,13 @@ REGEX_MENU = re.compile(cfg.COMMENT_R.format(cfg.SUBP_MENU),
                         re.UNICODE | re.IGNORECASE)
 
 
-class SubPageError(Exception):
-    pass
-
-
-class SubPage(object):
-    def __init__(self, content, filename):
+class Subpage(object):
+    def __init__(self, content, filename=None):
         self.content = content
         self.has_title = False
         self.title = ''
-        self.has_menu_id = False
-        self.menu_id = ''
+        self.has_menu = False
+        self.menu = ''
         match = REGEX_TITLE.search(content)
         if match:
             self.title = match.group(1)
@@ -31,11 +28,11 @@ class SubPage(object):
         raise NotImplementedError
 
 
-class HtmlSubPage(SubPage):
+class HtmlSubpage(Subpage):
     def get_html(self):
         return self.content
 
 
-class MarkdownSubPage(SubPage):
+class MarkdownSubpage(Subpage):
     def get_html(self):
         raise NotImplementedError  # TODO use markdown parser for converting
