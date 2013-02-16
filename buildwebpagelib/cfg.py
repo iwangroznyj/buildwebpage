a6dd857a9ae94551f6e8c4d51eec4fd55ff1c838
Johannes Englisch <cyberjoe0815@hotmail.com>
2013-02-16 18:36:20 +0100

    added new template class

---
+++ buildwebpagelib/cfg.py
@@ -0,0 +1,16 @@
+# default input and output files
+DEFAULT_FILE_PREFIX = '_'
+DEFAULT_TEMPLATE = 'template.html'
+BUILD_DEST = './build/'
+
+# html comment with and without regex
+COMMENT = '<!-- {0} -->'
+COMMENT_R = '<!--\s*{0}\s*-->'
+
+# template substitution strings
+TMPL_CONTENT = 'content'
+TMPL_TITLE = 'title'
+
+# subpage strings
+SUBP_TITLE = '<!--\s*title\s*:\s*(.*?)\s*-->'
+SUBP_MENU = '<!--\s*menu\s*:\s*(.*?)\s*-->'
---
+++ buildwebpagelib/template.py
@@ -0,0 +1,26 @@
+import re
+import buildwebpage.cfg as cfg
+
+
+REGEX_CONTENT = re.compile(cfg.COMMENT_R.format(cfg.TMPL_CONTENT),
+                           re.UNICODE | re.IGNORECASE)
+REGEX_TITLE = re.compile(cfg.COMMENT_R.format(cfg.TMPL_TITLE),
+                         re.UNICODE | re.IGNORECASE)
+
+ERR_NOSTR = 'template lacks substitution string \'{string}\'.'
+
+
+class TemplateError(Exception):
+
+
+class Template(object):
+    def __init__(self, content):
+        self.content = content
+        self.has_title = False
+        if not REGEX_CONTENT.search(content):
+            raise TemplateError()
+        if REGEX_TITLE.search(content):
+            self.has_title = True
+
+    def assemble_page(self, subpage):
+        raise NotImplementedError  # TODO implement subpages first
