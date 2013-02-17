04293818dbc0ee6adfb30d6737002f2738f581a0
Johannes Englisch <cyberjoe0815@hotmail.com>
2013-02-17 02:04:40 +0100

    implemented webpage generation

--- buildwebpagelib/cfg.py
+++ buildwebpagelib/cfg.py
@@ -5,8 +5,13 @@ BUILD_DEST = './build/'
 
 # html comment with and without regex
 COMMENT = '<!-- {0} -->'
-COMMENT_R = '<!--\s*{0}\s*-->'
-PROPERTY_R = '{0}\s*:\s*(.+?)'
+COMMENT_R = r'<!--\s*{0}\s*-->'
+PROPERTY_R = r'{0}\s*:\s*(.+?)'
+
+# menu item regex
+MENU_R = '\\<.*?<id=[\'"](\\w+)[\'"].*?\\>'
+# current menu item class
+MENU_CURRENT = 'class=\'menu_current\''
 
 # template substitution strings
 TMPL_CONTENT = 'content'
--- buildwebpagelib/template.py
+++ buildwebpagelib/template.py
@@ -1,14 +1,20 @@
 import re
 from . import cfg
+from . import warning
 
 
 REGEX_CONTENT = re.compile(cfg.COMMENT_R.format(cfg.TMPL_CONTENT),
                            re.UNICODE | re.IGNORECASE)
 REGEX_TITLE = re.compile(cfg.COMMENT_R.format(cfg.TMPL_TITLE),
                          re.UNICODE | re.IGNORECASE)
+REGEX_MENU = re.compile(cfg.MENU_R, re.UNICODE | re.IGNORECASE)
+
+WARN_SUB_TITLE = 'Subpage does not set title for template\'s title slot'
+WARN_TMP_TITLE = 'Template lacks title slot for title set by subpage'
+WARN_TMP_MENU = 'Template lacks menu item referenced by subpage'
 
 
-class TemplateNoContentError(Exception):
+class TemplateContentError(Exception):
     pass
 
 
@@ -17,9 +23,18 @@ class Template(object):
         self.content = content
         self.has_title = False
         if not REGEX_CONTENT.search(content):
-            raise TemplateNoContentError()
+            raise TemplateContentError()
         if REGEX_TITLE.search(content):
             self.has_title = True
 
-    def assemble_page(self, subpage):
-        raise NotImplementedError  # TODO implement subpages first
+    def build_page(self, subpage):
+        webpage = self.content
+        if self.has_title:
+            if subpage.has_title:
+                webpage = REGEX_TITLE.sub(subpage.title, webpage)
+            else:
+                warning.warnf(WARN_SUB_TITLE)
+        elif webpage.has_title:
+            warning.warnf(WARN_TMP_TITLE)
+        # TODO substitute menu string
+        return REGEX_CONTENT.sub(subpage.get_html(), webpage)
