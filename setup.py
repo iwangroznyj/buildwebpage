f58a50aa6ff0035e4f657d746d58c616689de0ae
Johannes Englisch <cyberjoe0815@hotmail.com>
2013-02-21 01:56:27 +0100

    added packaging

---
+++ setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+'''Buildwebpage setup script.
+
+Uses distutils in order to install the buildwebpage script or create
+distributable packages.
+
+'''
+
+
+from distutils.core import setup
+import buildwp.cfg
+
+
+# TODO url, download_url
+config = {'name': 'buildwebpage',
+          'version': buildwp.cfg.VERSION,
+          'author': 'Johannes Englisch',
+          'author_email': 'cyberjoe0815@hotmail.com',
+          'description': 'Build static webpages using a template',
+          'long_description': buildwp.cfg.DESCRIPTION,
+          'classifiers': ['Development Status :: 4 - Beta',
+                          'Environment :: Console',
+                          'Intended Audience :: Developers',
+                          'License :: OSI Approved :: MIT License',
+                          'Operating System :: OS Independent',
+                          'Programming Language :: Python :: 2',
+                          'Programming Language :: Python :: 2 :: 2.7',
+                          'Topic :: Internet :: WWW/HTTP :: Site Management'],
+          'requires': ['Markdown'],
+          'packages': ['buildwp'],
+          'scripts': ['buildwebpage']}
+
+setup(**config)
