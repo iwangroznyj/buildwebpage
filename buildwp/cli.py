316f1e3d5be8be45a5d0c0cbe26cb9b8740c0e77
Johannes Englisch <cyberjoe0815@hotmail.com>
2013-02-22 11:27:53 +0100

    changed command-line interface back to imperative style

--- buildwp/cli.py
+++ buildwp/cli.py
@@ -18,12 +18,6 @@ import glob
 from . import cfg
 
 
-# Default values
-DEFAULTS = {'template': cfg.DEFAULT_TEMPLATE,
-            'subpages': glob.glob(cfg.DEFAULT_SUBPAGES),
-            'dest': cfg.DEFAULT_DEST,
-            'conf': cfg.DEFAULT_CONF}
-
 # Command line help strings
 HLP_TEMPL = 'template for the webpage \
 (defaults to \'{0}\')'.format(cfg.DEFAULT_TEMPLATE)
@@ -40,87 +34,89 @@ CFG_SECTION = '[{section}]\n{options}'
 CFG_OPTION = '{option} = {value}'
 
 
-class Settings(dict):
-    '''Settings dictionary.
+def get_settings(args):
+    '''Create the dictionary and set options.
 
-    This class is a dictionary (i.e. a subclass of dict) which draws its
-    content from both the command-line and the config file.
+    :param args: command-line arguments
+    :type  args: list of str
+    :return:     application settings
+    :rtype:      dict
 
     '''
+    settings = {'template': cfg.DEFAULT_TEMPLATE,
+                'dest': cfg.DEFAULT_DEST,
+                'conf': cfg.DEFAULT_CONF}
+    cli_args = parse_commandline(args)
+    if 'conf' in cli_args:
+        settings['conf'] = cli_args['conf']
+    cfg_args = parse_configfile(settings['conf'])
+    settings.update(cfg_args)
+    settings.update(cli_args)
+    if not 'subpages' in settings:
+        settings['subpages'] = glob.glob(cfg.DEFAULT_SUBPAGES)
+    if 'gencfg' in cli_args:
+        write_configfile(settings, settings['conf'])
+    return settings
 
-    def __init__(self, args):
-        '''Create the dictionary and set options.
 
-        :param args: command-line arguments
-        :type  args: list of str
+def parse_commandline(args):
+    '''Parse command-line arguments.
 
-        '''
-        super(Settings, self).__init__(DEFAULTS)
-        cli_args = self.parse_commandline(args)
-        if 'conf' in cli_args:
-            self['conf'] = cli_args['conf']
-        cfg_args = self.parse_configfile(self['conf'])
-        self.update(cfg_args)
-        self.update(cli_args)
-        if 'gencfg' in cli_args:
-            self.write_configfile(self['conf'])
+    :param args: command-line arguments
+    :type  args: list of str
+    :return:     settings
+    :rtype:      dict
 
-    def parse_commandline(self, args):
-        '''Parse command-line arguments.
+    '''
+    parser = argparse.ArgumentParser(description=cfg.DESCRIPTION,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     argument_default=argparse.SUPPRESS)
+    parser.add_argument('template', nargs='?', help=HLP_TEMPL)
+    parser.add_argument('subpages', nargs='*', metavar='subpage',
+                        help=HLP_SUBPG)
+    parser.add_argument('-c', '--conf', metavar='FILE', help=HLP_CONF)
+    parser.add_argument('-d', '--dest', dest='dest', metavar='DIR',
+                        help=HLP_DEST)
+    parser.add_argument('--gen-config', dest='gencfg', action='store_true',
+                        help=HLP_GENCFG)
+    return parser.parse_args(args).__dict__
 
-        :param args: command-line arguments
-        :type  args: list of str
-        :return:     settings
-        :rtype:      dict
 
-        '''
-        parser = argparse.ArgumentParser(description=cfg.DESCRIPTION,
-                                         formatter_class=argparse.RawDescriptionHelpFormatter,
-                                         argument_default=argparse.SUPPRESS)
-        parser.add_argument('template', nargs='?', help=HLP_TEMPL)
-        parser.add_argument('subpages', nargs='*', metavar='subpage',
-                            help=HLP_SUBPG)
-        parser.add_argument('-c', '--conf', metavar='FILE', help=HLP_CONF)
-        parser.add_argument('-d', '--dest', dest='dest', metavar='DIR',
-                            help=HLP_DEST)
-        parser.add_argument('--gen-config', dest='gencfg', action='store_true',
-                            help=HLP_GENCFG)
-        return parser.parse_args(args).__dict__
+def parse_configfile(filename):
+    '''Parse settings in the config file.
 
-    def parse_configfile(self, filename):
-        '''Parse settings in the config file.
+    :param filename: name of the config file
+    :type  filename: str
+    :return:         settings
+    :rtype:          dict
 
-        :param filename: name of the config file
-        :type  filename: str
-        :return:         settings
-        :rtype:          dict
+    '''
+    config = ConfigParser.SafeConfigParser(allow_no_value=True)
+    config.read(filename)
+    content = dict()
+    if config.has_section(cfg.SECTION_CONFIG):
+        content.update((key, var)
+                       for key, var in config.items(cfg.SECTION_CONFIG)
+                       if key in ['template', 'dest'])
+    if config.has_section(cfg.SECTION_SUBPG):
+        content['subpages'] = config.options(cfg.SECTION_SUBPG)
+    return content
 
-        '''
-        config = ConfigParser.SafeConfigParser(allow_no_value=True)
-        config.read(filename)
-        content = dict()
-        if config.has_section(cfg.SECTION_CONFIG):
-            content.update((key, var)
-                           for key, var in config.items(cfg.SECTION_CONFIG)
-                           if key in DEFAULTS.keys())
-        if config.has_section(cfg.SECTION_SUBPG):
-            content['subpages'] = config.options(cfg.SECTION_SUBPG)
-        return content
 
-    def write_configfile(self, filename):
-        '''Write current settings to a config file.
+def write_configfile(settings, filename):
+    '''Write current settings to a config file.
 
-        :param filename: name of the config file
-        :type  filename: str
+    :param filename: name of the config file
+    :type  filename: str
 
-        '''
-        options = [CFG_OPTION.format(option=option, value=value)
-                   for option, value in self.items()
-                   if option in ['template', 'dest']]
-        settings = CFG_SECTION.format(section=cfg.SECTION_CONFIG,
+    '''
+    options = [CFG_OPTION.format(option=option, value=value)
+               for option, value in settings.items()
+               if option in ['template', 'dest']]
+    sec_settings = CFG_SECTION.format(section=cfg.SECTION_CONFIG,
                                       options='\n'.join(options))
-        subpages = CFG_SECTION.format(section=cfg.SECTION_SUBPG,
-                                      options='\n'.join(self['subpages']))
-        config = settings + '\n\n' + subpages
-        with open(filename, 'w') as cfgfile:
-            cfgfile.write(config)
+    sec_subpages = CFG_SECTION.format(section=cfg.SECTION_SUBPG,
+                                      options='\n'.join(settings['subpages']))
+    config = sec_settings + '\n\n' + sec_subpages
+    with open(filename, 'w') as cfgfile:
+        cfgfile.write(config)
