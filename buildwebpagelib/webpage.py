f3096da3f749cc46a0efeb86edff88ab42bf3182
Johannes Englisch <cyberjoe0815@hotmail.com>
2015-02-08 09:33:07 +0100

    fixed some obvious bugs..

--- buildwebpagelib/webpage.py
+++ buildwebpagelib/webpage.py
@@ -1,16 +1,16 @@
-from os import mkdirs
+from os import mkdir
 from os.path import join, isdir, basename
 
 
 def create_webpage(templ, subpages, destination):
     if not isdir(destination):
-        mkdirs(destination)
+        mkdir(destination)
     for subpage in subpages:
         output_file = join(destination, basename(subpage.filename))
         composed = templ.insert_subpage(subpage)
         print(templ.filename, ' + ', subpage.filename, ' => ', output_file)
         try:
-            with open(output_file, 'w'):
-                output_file.write(composed)
+            with open(output_file, 'w') as f:
+                f.write(composed)
         except IOError as error:
             warning.warnf(error)
