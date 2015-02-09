8fefcecb48cd43015aafb551a292c0c1ca5a4343
Johannes Englisch <cyberjoe0815@hotmail.com>
2015-02-09 15:34:51 +0100

    deleted the cfg module

--- buildwebpagelib/cfg.py
+++
@@ -1,4 +0,0 @@
-RE_TEMPL_TITLE = _RE_COMMENT.format(SUBST_TITLE)
-RE_TEMPL_CONTENT = _RE_COMMENT.format(SUBST_CONTENT)
-# We don't know what the menuid will be, so the regex gets a replacement field
-RE_TEMPL_MENUID = _RE_ATTRIBUTE.format(SUBST_ATTRIBUTE, '{0}')
