1fe0d80416e5ff61a4033786a71d21c20a1f2f33
Johannes Englisch <cyberjoe0815@hotmail.com>
2015-02-10 13:16:46 +0100

    added test for menu item replacement

--- tests.py
+++ tests.py
@@ -9,10 +9,16 @@ class Template(unittest.TestCase):
         tmpl = bw.Template('before <!-- CONTENT --> after')
         self.assertEqual(tmpl.insert_subpage(subpage), 'before content after')
 
-    def test_instert_title(self):
+    def test_insert_title(self):
         subpage = bw.Subpage('<!-- TITLE: le title -->')
-        tmpl = bw.Template('before <!-- TITLe --> after')
+        tmpl = bw.Template('before <!-- TITLE --> after')
         self.assertEqual(tmpl.insert_subpage(subpage), 'before le title after')
+
+    def test_insert_menu_item(self):
+        subpage = bw.Subpage('<!-- MENU_ID: subpage -->')
+        tmpl = bw.Template('before <span id="subpage"> after')
+        self.assertEqual(tmpl.insert_subpage(subpage),
+                         'before <span id="subpage" class=\'menu-current\' > after')
 
 
 if __name__ == '__main__':
